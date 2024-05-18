855c6c5bbb5f2ce7468244e2b6976b1e26c328b0
강수림 <eathergs012@gmail.com>
2024-05-18 12:03:38 +0900

    [2024.05.18] FIX api_gateway_properties

--- api_gateway/src/main/java/com/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
